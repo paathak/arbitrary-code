e844cedf7ffd8acbb0e0500af793e3554e1e7918
paathak <38093288+paathak@users.noreply.github.com>
2026-05-27 06:10:48 +0530

    Refactor remote.lua for improved organization

--- remote.lua
+++ remote.lua
@@ -1,67 +1,117 @@
 local keyboard = libs.keyboard;
-local win = libs.win;
-local utf8 = libs.utf8;
+local win      = libs.win;
+local utf8     = libs.utf8;
+local mouse    = libs.mouse;
 
-
-
-
-
+-- ==========================================
+-- SYSTEM & POWER CONTROLS
+-- ==========================================
 --@help Put system in sleep state
 actions.sleep = function ()
-	os.execute("systemctl suspend")
-end
-
---@help Close current tab
-actions.close_app = function()
-	--actions.switch();
-	keyboard.stroke("alt", "f4");
+    os.execute("systemctl suspend")
 end
 
 --@help System shutdown
 actions.shutdown = function ()
-    os.execute("systemctl poweroff");
+    os.execute("systemctl poweroff")
 end
 
+--@help Close current app
+actions.close_app = function()
+    keyboard.stroke("alt", "f4")
+end
+
+-- ==========================================
+-- BROWSER & NAVIGATION CONTROLS
+-- ==========================================
 --@help Open new tab
 actions.new_tab = function()
-	--actions.switch();
-	keyboard.stroke("control", "T");
+    keyboard.stroke("control", "T")
 end
 
 --@help Type address
 actions.address = function()
-	--actions.switch();
-	keyboard.stroke("control", "L");
-	device.keyboard();
+    keyboard.stroke("control", "L")
+    device.keyboard()
 end
 
 --@help Close current tab
 actions.close_tab = function()
-	--actions.switch();
-	keyboard.stroke("control", "W");
+    keyboard.stroke("control", "W")
 end
 
+--@help Launch Home site
+actions.launch_home = function ()
+    keyboard.stroke("alt", "Home")
+end
+
+--@help Scroll page up
+actions.scroll_up = function()
+    keyboard.stroke("up")
+end
+
+--@help Scroll page down
+actions.scroll_down = function()
+    keyboard.stroke("down")
+end
+
+--@help Scroll page up (PgUp)
+actions.pg_up = function()
+    keyboard.stroke("pgup")
+end
+
+--@help Scroll page down (PgDn)
+actions.pg_down = function()
+    keyboard.stroke("pgdown")
+end
+
+--@help Fullscreen view
+actions.fullscreen = function()
+    keyboard.stroke("F")
+end
+
+--@help Windowed view
+actions.window = function()
+    keyboard.stroke("escape")
+end
+
+-- ==========================================
+-- MEDIA CONTROLS
+-- ==========================================
 --@help Seek forward
 actions.forward = function()
-	-- actions.switch();
-	keyboard.stroke("right");  
-end
-
---@help Toggle playback state
-actions.play_pause = function()
-	-- actions.switch();
-	keyboard.stroke("space");
+    keyboard.stroke("right")
 end
 
 --@help Seek backward
 actions.rewind = function()
-	-- actions.switch();
-	keyboard.stroke("left");
+    keyboard.stroke("left")
 end
 
-dragging = false
-local mouse = libs.mouse
+--@help Toggle playback state
+actions.play_pause = function()
+    keyboard.stroke("space")
+end
 
+--@help Raise volume
+actions.volume_up = function()
+    keyboard.stroke("volumeup")
+end
+
+--@help Lower volume
+actions.volume_down = function()
+    keyboard.stroke("volumedown")
+end
+
+--@help Mute volume
+actions.volume_mute = function()
+    keyboard.stroke("volumemute")
+end
+
+-- ==========================================
+-- MOUSE & TOUCHPAD LOGIC
+-- ==========================================
+dragging = false
 local SPEED = 2
 
 actions.tap = function ()
@@ -93,71 +143,13 @@ actions.delta = function (id, x, y)
 end
 
 actions.double = function ()
-	mouse.double("left")
+    mouse.double("left")
 end
 
 actions.left = function ()
-	mouse.click("left")
+    mouse.click("left")
 end
 
 actions.right = function ()
-	mouse.click("right")
-end
-
-
---@help Launch Home site
-actions.launch_home = function ()
-   keyboard.stroke("alt", "Home");
-end
-
-
---@help Raise volume
-actions.volume_up = function()
-	keyboard.stroke("volumeup");
-end
-
---@help Lower volume
-actions.volume_down = function()
-	keyboard.stroke("volumedown");
-end
-
---@help Mute volume
-actions.volume_mute = function()
-	keyboard.stroke("volumemute");
-end
-
---@help Scroll page up
-actions.scroll_up = function()
-	-- actions.switch();
-	keyboard.stroke("up");
-end
-
---@help Scroll page down
-actions.scroll_down = function()
-	-- actions.switch();
-	keyboard.stroke("down");
-end
-
---@help Fullscreen view
-actions.fullscreen = function()
-	-- actions.switch();
-	keyboard.stroke("F");
-end
-
---@help Windowed view
-actions.window = function()
-	-- actions.switch();
-	keyboard.stroke("escape");
-end
-
---@help Scroll page up
-actions.pg_up = function()
-	-- actions.switch();
-	keyboard.stroke("pgup");
-end
-
---@help Scroll page down
-actions.pg_down = function()
-	-- actions.switch();
-	keyboard.stroke("pgdown");
+    mouse.click("right")
 end
